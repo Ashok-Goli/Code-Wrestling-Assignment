15d80c19afd653c3bd330b4c89f967da0f5ff896
Ashok Goli <142201695+Ashok-Goli@users.noreply.github.com>
2023-11-06 13:35:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("getInfoButton").addEventListener("click", function() {
+    // You can add your logic here to capture user's name and phone number
+    // and handle the "Get More Information" functionality.
+});
